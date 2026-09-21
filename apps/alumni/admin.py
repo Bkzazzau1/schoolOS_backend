87bf22dcd45441e90091f9632d1896b5f35dcd2e
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-21 17:39:14 +0100

    Protect Alumni verification audit in Django admin

--- apps/alumni/admin.py
+++ apps/alumni/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import AlumniProfile
+from .models import AlumniProfile, AlumniVerificationEvent
 
 
 @admin.register(AlumniProfile)
@@ -21,3 +21,34 @@ class AlumniProfileAdmin(admin.ModelAdmin):
         "profession",
         "organisation",
     )
+    readonly_fields = (
+        "verification_status",
+        "submitted_at",
+        "reviewed_at",
+        "verification_note",
+        "verified_at",
+        "verified_by",
+        "created_at",
+        "updated_at",
+    )
+
+
+@admin.register(AlumniVerificationEvent)
+class AlumniVerificationEventAdmin(admin.ModelAdmin):
+    list_display = ("profile", "event", "actor", "at")
+    list_filter = ("event", "profile__school")
+    search_fields = (
+        "profile__membership__user__email",
+        "profile__admission_number",
+        "note",
+    )
+    readonly_fields = ("profile", "event", "actor", "note", "at")
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
