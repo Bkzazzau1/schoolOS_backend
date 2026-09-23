c31f8b12e83a2acf8e9799c9b26041a157f5f7b9
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-23 22:28:42 +0100

    Add account email verification fields

---
+++ apps/accounts/migrations/0002_user_email_verification.py
@@ -0,0 +1,35 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("accounts", "0001_initial"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="user",
+            name="email_verified_at",
+            field=models.DateTimeField(blank=True, null=True),
+        ),
+        migrations.AddField(
+            model_name="user",
+            name="email_verification_code_hash",
+            field=models.CharField(blank=True, max_length=64),
+        ),
+        migrations.AddField(
+            model_name="user",
+            name="email_verification_expires_at",
+            field=models.DateTimeField(blank=True, null=True),
+        ),
+        migrations.AddField(
+            model_name="user",
+            name="email_verification_sent_at",
+            field=models.DateTimeField(blank=True, null=True),
+        ),
+        migrations.AddField(
+            model_name="user",
+            name="email_verification_attempts",
+            field=models.PositiveSmallIntegerField(default=0),
+        ),
+    ]
