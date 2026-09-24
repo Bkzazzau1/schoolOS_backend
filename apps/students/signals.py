238e5d63b34d79aac518c6d0ab9c04c3f0c01f55
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-24 09:32:45 +0100

    Refresh student class sync after enrollment changes

--- apps/students/signals.py
+++ apps/students/signals.py
@@ -5,6 +5,8 @@ from apps.alumni.models import AlumniVerificationEvent
 from apps.billing.models import SchoolBillingMeterSnapshot
 
 from .alumni_bridge import graduate_canonical_student_from_alumni_transition
+from .models import EnrollmentStatus, StudentEnrollment
+from .student_sync import publish_student_class_link
 
 
 @receiver(post_save, sender=AlumniVerificationEvent)
@@ -15,6 +17,15 @@ def mirror_alumni_transition_to_student_roster(sender, instance, created, **kwar
         instance.profile,
         actor=instance.actor,
     )
+
+
+@receiver(post_save, sender=StudentEnrollment)
+def refresh_student_class_link(sender, instance, created, **kwargs):
+    """Keep the Student workspace on the canonical active class after movement."""
+
+    if instance.status != EnrollmentStatus.ACTIVE:
+        return
+    publish_student_class_link(instance.student)
 
 
 @receiver(pre_save, sender=SchoolBillingMeterSnapshot)
