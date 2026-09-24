bf7e50b59569962381bc8d9ad4b9546247a91da3
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-24 09:36:48 +0100

    Apply dedicated login throttling

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -12,6 +12,7 @@ from .onboarding import register_proprietor_account
 from .throttles import (
     EmailVerificationConfirmThrottle,
     EmailVerificationSendThrottle,
+    LoginThrottle,
     RegistrationThrottle,
 )
 
@@ -21,6 +22,7 @@ class SchoolOSTokenView(APIView):
 
     authentication_classes = ()
     permission_classes = (AllowAny,)
+    throttle_classes = (LoginThrottle,)
 
     def post(self, request):
         serializer = SchoolOSTokenSerializer(data=request.data)
